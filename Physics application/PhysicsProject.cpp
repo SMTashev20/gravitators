b85d36cd73a7bbd66d1ce0a12e2ccc74df5a8282
VZKardzheva20 <VZKardzheva20@codingburgas.bg>
2021-11-21 14:34:02 +0200

    Update exercise base

--- Physics application/PhysicsProject.cpp
+++ Physics application/PhysicsProject.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-#include <cstring> 
+#include <string> 
 #include <ctime>
-#include<windows.h>
+#include <windows.h>
+#include <conio.h>
+
+#define COLOR_AQUA 3
+#define COLOR_GRAY 8
+#define COLOR_WHITE 7
+#define COLOR_YELLOW 6
 
 using namespace std;
-void outputPosition(int x, int y)
+
+void setOutputPosition(short x, short y)
 {
 	COORD position;
 	position.X = x;
@@ -25,140 +32,64 @@ void color(int color)
 	else {}
 }
 
-void printMap() {
-	outputPosition(0, 1);
-	color(3);
-	cout << "      ________________________________________________________________________________" << endl;
-	cout << "     |  ____________________________________________________________________________  |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |                                                                            | |" << endl;
-	cout << "     | |____________________________________________________________________________| |" << endl;
-	cout << "     |________________________________________________________________________________|" << endl;
-
-	color(8);
-	outputPosition(8, 4);
-	cout << "                         *        '      ";
-	color(6);
-	cout << "     .      " << endl;
-	color(8);
-	outputPosition(8, 5);
-	cout << "   *         '       +         *";
-	cout << "             .         '                ";
-	color(6);
-	cout << "*" << endl;
-	color(8);
-	outputPosition(8, 6);
-	cout << "    .                 .         *                                   '       " << endl;
-	outputPosition(8, 8);
-	cout << "                         *        '                     *                   " << endl;
-	outputPosition(8, 9);
-	cout << "    *                                                                *     " << endl;
-	outputPosition(8, 11);
-	cout << "                                          '                                " << endl;
-	outputPosition(8, 12);
-	cout << "   *            *          .                                +               " << endl;
-	outputPosition(8, 14);
-	cout << "    .                 .                                    .             " << endl;
-	outputPosition(8, 15);
-	cout << "           +                                                         .    " << endl;
-	outputPosition(8, 16);
-	cout << "                                 '                '     *                  " << endl;
-	color(6);
-	outputPosition(8, 17);
-	cout << "   +                                                                 +     " << endl;
-	color(8);
-	outputPosition(8, 18);
-	cout << "      .          +                                                         " << endl;
-	outputPosition(8, 19);
-	cout << "                              +                               *          " << endl;
-	outputPosition(8, 20);
-	cout << "           +                                                              " << endl;
-	outputPosition(8, 21);
-	cout << "                      '                                *                  " << endl;
-	outputPosition(8, 22);
-	cout << "         +         *             .         '         *                 '  " << endl;
-	outputPosition(8, 23);
-	cout << "                                                                   *      " << endl;
-	outputPosition(8, 24);
-	cout << "   *            *          .                                  +           " << endl;
-	outputPosition(8, 25);
-	cout << "                    '                        ";
-	color(6);
-	cout << "+                         ";
-	color(8);
-	cout << "+   " << endl;
-	color(6);
-	outputPosition(16, 5);
-	cout << " __   __                    _               _          _ ";
-	outputPosition(16, 6);
-	cout << " \\ \\ / /_ _ _   _     _ __ | |__  _   _ ___(_) ___ ___| |";
-	outputPosition(16, 7);
-	cout << "  \\ V / _` | | | |   | '_ \\| '_ \\| | | / __| |/ __/ __| |";
-	outputPosition(16, 8);
-	cout << "   | | (_| | |_| |_  | |_) | | | | |_| \\__ \\ | (__\\__ \\_|";
-	outputPosition(16, 9);
-	cout << "   |_|\\__,_|\\__, ( ) | .__/|_| |_|\\__, |___/_|\\___|___(_)";
-	outputPosition(16, 10);
-	cout << "            |___/|/  |_|          |___/                 ";
-	outputPosition(8, 25);
-	cout << "TEAM GRAVITATORS" << endl;
+setOutputPosition(8, 24);
+cout << "   *            *          .                                  +           " << endl;
+setOutputPosition(8, 25);
+cout << "                    '                        ";
+setConsoleColor(6);
+cout << "+                         ";
+setConsoleColor(8);
+cout << "+   " << endl;
+setConsoleColor(6);
+setOutputPosition(16, 5);
+cout << " __   __                    _               _          _ ";
+setOutputPosition(16, 6);
+cout << " \\ \\ / /_ _ _   _     _ __ | |__  _   _ ___(_) ___ ___| |";
+setOutputPosition(16, 7);
+cout << "  \\ V / _` | | | |   | '_ \\| '_ \\| | | / __| |/ __/ __| |";
+setOutputPosition(16, 8);
+cout << "   | | (_| | |_| |_  | |_) | | | | |_| \\__ \\ | (__\\__ \\_|";
+setOutputPosition(16, 9);
+cout << "   |_|\\__,_|\\__, ( ) | .__/|_| |_|\\__, |___/_|\\___|___(_)";
+setOutputPosition(16, 10);
+cout << "            |___/|/  |_|          |___/                 ";
+setOutputPosition(8, 25);
+cout << "TEAM GRAVITATORS" << endl;
 }
 
 int main()
 {
-	printMap();
-	color(7);
-	outputPosition(28, 12);
-	cout << "Welcome! Please pick your game." << endl;
-	outputPosition(22, 14);
-	cout << "1. Option" << endl;
-	outputPosition(38, 14);
-	cout << "2. Option" << endl;
-	outputPosition(54, 14);
-	cout << "3. Option" << endl;
-	outputPosition(22, 16);
-	cout << "4. Option" << endl;
-	outputPosition(38, 16);
-	cout << "5. Option" << endl;
-	outputPosition(54, 16);
-	cout << "6. Option" << endl;
-	outputPosition(22, 18);
-	cout << "7. Option" << endl;
-	outputPosition(38, 18);
-	cout << "8. Option" << endl;
-	outputPosition(54, 18);
-	cout << "9. Option" << endl;
+    printMap();
+    setConsoleColor(7);
+    setOutputPosition(28, 12);
+    cout << "Welcome! Please pick your game." << endl;
+    setOutputPosition(22, 14);
+    cout << "1. Option" << endl;
+    setOutputPosition(38, 14);
+    cout << "2. Option" << endl;
+    setOutputPosition(54, 14);
+    cout << "3. Option" << endl;
+    setOutputPosition(22, 16);
+    cout << "4. Option" << endl;
+    setOutputPosition(38, 16);
+    cout << "5. Option" << endl;
+    setOutputPosition(54, 16);
+    cout << "6. Option" << endl;
+    setOutputPosition(22, 18);
+    cout << "7. Option" << endl;
+    setOutputPosition(38, 18);
+    cout << "8. Option" << endl;
+    setOutputPosition(54, 18);
+    cout << "9. Option" << endl;
 
-	color(6);
-	int game;
-	outputPosition(41, 20);
-	cin >> game;
-	switch (game)
-	{
-	case 1:
-	{
-	}
-	}
+    setConsoleColor(6);
+    int game;
+    setOutputPosition(41, 20);
+    cin >> game;
+    switch (game)
+    {
+    case 1:
+    {
+    }
+    }
 }
-
--- Physics application/exercise1.cpp
+++ Physics application/exercise1.cpp
@@ -1,163 +1,147 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
+void ignoreUserInput()
 {
-    float V, a, t;
-    float V0, s;
+    cin.clear(); // removes cin error flag
+    cin.ignore(INT_MAX, '\n');  // ignore last input
+}
 
-    int arr[26];
-    int arr1[26] = { 0 * 10 + (8 * 10 * 10) / 2 };
+void inputNumber(double& number)
+{
+    cin >> number;
 
-    bool flag = true;
-
-    char sym, sym2;
-
-    while (flag == true)
+    while (cin.fail())
     {
-        cout << "Enter the unit we are searching: (s/V)" << endl;
-
-    jump:
-        cin >> sym;
-        if (sym == 's')
-        {
-            // s = V0 * t + (a * t * t) / 2;
-
-            cout << "Enter the initial velocity of the body (Vo):" << endl;
-            cin >> V0;
-
-            cout << "Enter the time interval (t):" << endl;
-            cin >> t;
-
-            cout << "Enter the acceleration of the body (a):" << endl;
-            cin >> a;
-
-            cout << "Our formula is s = Vo * t + (a * t * t) / 2" << endl;
-
-            cout << "First we replace the values:" << endl;
-
-            cout << "s = " << V0 << "*" << t << " + " << "(" << a << "*" << t * t << ")" << "/2" << endl;
-
-            cout << "Then we calculate our eqalation:" << endl;
-
-            cout << "s = " << V0 * t << " + " << (a * t * t) / 2 << endl;
-
-            cout << "s = " << V0 * t + (a * t * t) / 2 << " m" << endl;
-            flag = false;
-        }
-
-        else if (sym == 'V')
-        {
-
-            // V = V0 + a * t;
-
-            cout << "Enter the initial velocity of the body (Vo):" << endl;
-            cin >> V0;
-
-            cout << "Enter the acceleration of the body (a):" << endl;
-            cin >> a;
-
-            cout << "Enter the time interval (t):" << endl;
-            cin >> t;
-
-            cout << "Our formula is V = Vo + a * t" << endl;
-
-            cout << "First we replace the values:" << endl;
-
-            cout << "V = " << V0 << " + " << a << "*" << t << endl;
-
-            cout << "Then we calculate our eqalation:" << endl;
-
-            cout << "V = " << V0 << " + " << a * t << endl;
-
-            cout << "V = " << V0 + a * t << " m/s" << endl;
-            flag = false;
-        }
-        else
-        {
-            cout << "Enter right unit." << endl;
-            goto jump;
-        }
+        ignoreUserInput();
+        cout << "You can only enter numbers.\n";
+        cout << "Enter a number.\n";
+        cin >> number;
     }
+}
 
-    t = 0;
-    V0 = 0;
-    s = 0;
-    a = 0;
-    V = 0;
+void distanceCalculator()
+{
+    double V0, t, a;
+    cout << "Enter the initial velocity of the body (Vo):" << endl;
+    inputNumber(V0);
 
-    cout << "Do you want to do another exercise or slove a problem? (e/p)" << endl;
-    cin >> sym2;
+    cout << "Enter the time interval (t):" << endl;
+    inputNumber(t);
 
-    if (sym2 == 'e')
+    cout << "Enter the acceleration of the body (a):" << endl;
+    inputNumber(a);
+
+    cout << "Our formula is s = Vo * t + (a * t * t) / 2" << endl;
+    cout << "First we replace the values:" << endl;
+    cout << "s = " << V0 << " * " << t << " + " << "(" << a << " * " << t * t << ")" << " / 2" << endl;
+    cout << "Then we calculate our eqalation:" << endl;
+    cout << "s = " << V0 * t << " + " << (a * t * t) / 2 << endl;
+    cout << "s = " << V0 * t + (a * t * t) / 2 << " m" << endl;
+}
+
+void velocityCalculator()
+{
+    double V0, t, a;
+    cout << "Enter the initial velocity of the body (Vo):" << endl;
+    inputNumber(V0);
+
+    cout << "Enter the acceleration of the body (a):" << endl;
+    inputNumber(a);
+
+    cout << "Enter the time interval (t):" << endl;
+    inputNumber(t);
+
+    cout << "Our formula is V = Vo + a * t" << endl;
+    cout << "First we replace the values:" << endl;
+    cout << "V = " << V0 << " + " << a << " * " << t << endl;
+    cout << "Then we calculate our eqalation:" << endl;
+    cout << "V = " << V0 << " + " << a * t << endl;
+    cout << "V = " << V0 + a * t << " m/s" << endl;
+}
+
+void velocityDistanceCalculator(char symbol)
+{
+    if (symbol == 's')
     {
-        goto jump;
+        distanceCalculator();
     }
     else
     {
-        cout << "Now, try to slove this problem!" << endl;
+        velocityCalculator();
+    }
+}
 
-        cout << "From rest, a car accelerated at 8 m/s2 for 10 seconds. What is the position of the car at the end of the 10 seconds? (s)" << endl;
+void exerciseOne(bool& pointAdd)
+{
+    double answer;
+    cout << "Now, try to solve this problem!" << endl;
+    cout << "From rest, a car accelerated at 8 m/s2 for 10 seconds. What is the position of the car at the end of the 10 seconds? (s)" << endl;
+    cout << "V0 = ?m (example input: 123)";
+    cin >> answer;
+    if (answer == 123)
+    {
+        pointAdd = true;
+    }
+    else
+    {
+        pointAdd = false;
+    }
+}
 
-    wrong:
-        cout << "V0 = ";
-        cin >> V0;
-        if (V0 == 0)
+int main()
+{
+    char sym;
+    if (sym == 'c')
+    {
+        cout << "Enter the unit we are searching (s/V)" << endl;
+        cin >> sym;
+        velocityDistanceCalculator(sym);
+    }
+    else if (sym == 'p')
+    {
+        exerciseOne(isCorrect);
+        if (isCorrect == true)
         {
-            cout << "a = ";
-            cin >> a;
-            if (a == 8)
-            {
-                cout << "t = ";
-                cin >> t;
-                if (t == 10)
-                {
-                    cout << "We have the formula ";
-                    cout << "s = V0 * t + (a * t * t) / 2" << endl;
-                    cout << "s = ";
-                    for (int i = 0; i < 26; i++)
-                    {
-                        cin >> arr[i];
-                    }
-
-                    for (int i = 0; i < 26; i++)
-                    {
-                        if (arr[i] == arr1[i])
-                        {
-                            cout << "bravo be";
-                            break;
-                        }
-                        else
-                        {
-                            cout << "NE";
-                        }
-
-                    }
-
-
-                }
-                else
-                {
-                    goto wrongAgain;
-                }
-
-            }
-            else
-            {
-                goto wrongAgain;;
-            }
+            cout << "That's correct!";
+            correctExerciseCount++;
         }
-
-        else
+        else if (isCorrect == false)
         {
-        wrongAgain:
-            cout << "Wrong. Try again." << endl;
-            goto wrong;
+            cout << "That's wrong!";
         }
+    }
+    cout << "Would you like to go to the next exercise? (y/n)SS";
+    cin >> sym;
 
-
-
+    while (sym != 'c' && sym != 'p')
+    {
+        cout << "Please enter a valid input!";
+        cout << "Would you like to use the calculator or solve a problem? (c/p)" << endl;
+        cin >> sym;
     }
 
+    int correctExerciseCount = 0;
+    bool isCorrect;
+    if (sym == 'c')
+    {
+        cout << "Enter the unit we are searching (s/V)" << endl;
+        cin >> sym;
+        velocityDistanceCalculator(sym);
+    }
+    else if (sym == 'p')
+    {
+        exerciseOne(isCorrect);
+        if (isCorrect == true)
+        {
+            cout << "That's correct!";
+            correctExerciseCount++;
+        }
+        else if (isCorrect == false)
+        {
+            cout << "That's wrong!";
+        }
+    }
+
+    cout << "You got " << correctExerciseCount << " out of *total exercises* points!";
 }
