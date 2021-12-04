3a5c4f9435455f0b0ab2a1777693cd82ec9d0aaf
StanislavTashev06 <85334165+SMTashev20@users.noreply.github.com>
2021-12-04 18:37:35 +0200

    Add questions to the quiz

--- Gravitators/Source Files/Back-End/Quiz.cpp
+++ Gravitators/Source Files/Back-End/Quiz.cpp
@@ -191,14 +191,90 @@ void QuizRecap()
 	outputOptions("Controls:", "W, D - up, down", "Z - confirm", "N - continue");
 	printTutorialUser();
 
-	outputOptions("Question?", "Answer", "no", "no"); //Correct rows: 9, 11 and 13
+	outputOptions("The change in velocity in a given time period is?", "Vi", "Vf", "a"); //Correct rows: 9, 11 and 13
+	printUser(pointsTotal, 13);
+
+    outputOptions("When the velocity of an object does not change, \n what happens to its acceleration?", "Nothing, it does accelerate", "It is a constant acceleration", "It is a changing acceleration");
 	printUser(pointsTotal, 9);
 
-    outputOptions("Question?", "no", "Answer", "no");
+	outputOptions("When an object falls in physics world, what happens?", "It accelerates at a non-constant rate until it hits the ground", "It accelerates at a constant rate until it hits the ground", "It accelerates at a constant rate until it reaches terminal velocity");
 	printUser(pointsTotal, 11);
 
-	outputOptions("Question?", "no", "no", "Answer");
+	outputOptions("Which would hit the ground first if dropped from the same height in a vacuum—a feather or a metal bolt?", "the feather", "the metal bolt", "They would hit the ground at the same time.");
 	printUser(pointsTotal, 13);
+
+	outputOptions("How is displacement different from distance?", "displacement is a vector, it must have magnitude and direction", "displacement is a vector, it must have magnitude only", "displacement is a scalar, it must have magnitude and direction");
+	printUser(pointsTotal, 9);
+
+	outputOptions("The SI unit for acceleration is?", "mph", "ft/sec2", "m/s2");
+	printUser(pointsTotal, 13);
+
+	outputOptions("What is the SI unit used to measure force ? ", "pounds ", "newtons  ", "grams");
+	printUser(pointsTotal, 11);
+
+	outputOptions("What's the formula for Newton's second law of motion (Force)?", "F = m - a", "F = a / m", "F= m * a");
+	printUser(pointsTotal, 13);
+
+	outputOptions("Which is the unit for pressure", "Pascal", "Newton", "Gram");
+	printUser(pointsTotal, 9);
+
+
+	outputOptions("When force increases...", "pressure increases", "pressure decreases", "pressure stays the same");
+	printUser(pointsTotal, 9);
+
+	outputOptions("Density is defined as", "mass/volume", "volume * mass", "volume + mass");
+	printUser(pointsTotal, 9);
+
+
+	outputTotalPoints(pointsTotal);
+
+	setOutputPosition(11, 32);
+	cout << endl << endl;
+	setColor(COLOR_BLACK);
+}
+
+void QuizDensity()
+{
+	printGameFrame_Exercises();
+	printStars_Exercises();
+
+	int pointsTotal = 0;
+	outputOptions("Controls:", "W, D - up, down", "Z - confirm", "N - continue");
+	printTutorialUser();
+
+	outputOptions("What two types of measurements make up DENSITY?", "Mass and volume", "Temperature and mass", "Grams and Centimetres"); //Correct rows: 9, 11 and 13
+	printUser(pointsTotal, 9);
+
+	outputOptions("Jack has a rock. The rock has a mass of 14g and a volume of 2cm3. What is the density of the rock?", "7mL", "7 g/cm^3", "1/7 g/cm^3");
+	printUser(pointsTotal, 11);
+
+	outputOptions("If an object has a density of .6 g/mL and you put it into water, will it sink or float?", "sink", "float", "none of them is correct");
+	printUser(pointsTotal, 11);
+
+	outputOptions("If an object has a density of 3.6 g/mL and you put it into water, will it sink or float?", "sink", "float", "none of them is correct");
+	printUser(pointsTotal, 13);
+
+	outputOptions("Frank has an eraser. It has a mass of 4g, and a volume of 2cm^3. What is its density?", "8 g/cm^3", "2 g/cm^3", "24 g/cm^3");
+	printUser(pointsTotal, 9);
+
+	outputOptions("What units are used to measure mass?", "g", "cm^3", "g/cm");
+	printUser(pointsTotal, 9);
+
+	outputOptions("What is the formula for density? ", "density = mass * volume ", "density = mass / volume – correct  ", "density = mass + volume");
+	printUser(pointsTotal, 11);
+
+	outputOptions("Helium balloons rise on earth because:", "helium is less dense than air.", "helium is denser than air.", "helium is denser than air.");
+	printUser(pointsTotal, 9);
+
+	outputOptions("The density of a specific type of material never changes. ", "True", "False", "None of them");
+	printUser(pointsTotal, 9);
+
+	outputOptions("Why do ice cubes float in a glass of water", "the ice cubes are more dense than the water", "the water is more dense than the glass", "the water is more dense than the ice cubes");
+	printUser(pointsTotal, 13);
+
+	outputOptions("Why do objects sink or float in H2O?", "Because their densities are higher or lower than compared to water", " Because their densities are using gravity to pull down", "Because their densities are heavier or lighter");
+	printUser(pointsTotal, 9);
+
 
 	outputTotalPoints(pointsTotal);
 
